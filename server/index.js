3cbccf17d3ba9cf809e27dca144159c1792c507e
s-hayashi1104 <shibahas.has@gmail.com>
2018-11-13 01:32:36 +0900

    途中経過 DBコネクション処理は途中、setup部分がまだ手をつけていない SQLの部分は完了

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import getConnection from './db/connection';
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config');
@@ -7,10 +8,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/api/items', (req, res) => {
+  const connection = getConnection.connect();
+  connection.query('select id, name, price, image_url from ‘items’;',
+    function (err, rows, fields) {
+      res.json({
+        row,
+        fields,
+      });
+  });
+  
+});
+
 setup(app);
 
 app.listen(config.port, () =>
-  console.info(`server started on port ${config.port}`),
+  console.info(`server started on port ${config.port}`)
 );
 
 app.use((err, req, res) => {

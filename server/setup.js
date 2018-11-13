580539d4cde7c9007c28801066df8c498bc172ba
s-hayashi1104 <shibahas.has@gmail.com>
2018-11-14 01:32:53 +0900

    async await見直し

--- server/setup.js
+++ server/setup.js
@@ -2,16 +2,12 @@ const db = require('./db/connection');
 
 const setup = function(app) {
   app.get('/api/items', async(req, res) => {
-    const connection = await db.getConnection;
-    console.log(connection);
-    connection.query('select id, name, price, image_url from ‘items’;',
-      function(err, rows, fields) {
-        console.log(rows);
-        res.json({
-          rows,
-          fields,
-        });
-      });
+    const connection = await db.getConnection();
+    const [row, field] = await connection.query('select `id`, `name`, `price`, `image_url` from `items`');
+    res.json({
+      row,
+      field
+    });
   });
 };
 

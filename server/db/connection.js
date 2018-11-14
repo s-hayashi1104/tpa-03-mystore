487c0144496bbad0f4bda03faef689a24e9e7a06
s-hayashi1104 <shibahas.has@gmail.com>
2018-11-14 21:29:27 +0900

    変数で受けずそのままreturnするように修正

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,28 +2,11 @@ const mysql = require('mysql2/promise');
 
 module.exports = {
   getConnection: (async() => {
-    const connection = await mysql.createConnection({
+    return await mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: 'Sju$34Tuf62Erqw1',
       database: 'my_store',
     });
-    return connection;
   }),
 };
-
-/**
-
-sample query using this mysql connection:
-
-const db = require('./db/connection');
-
-db.getConnection()
-  .then(dbConnection => {
-    return dbConnection.query('select * from items');
-  })
-  .then(([rows]) => {
-    console.log(rows); // log each row selected
-  });
-
-*/

e2964661b839745439c7d92160353b54446d19dc
s-hayashi1104 <shibahas.has@gmail.com>
2018-11-19 22:54:39 +0900

    validate追加

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -3,8 +3,14 @@
     <h1>Admin Page</h1>
     <p>See the <a href="#" v-on:click="handleStoreClick">Store</a></p>
     <form @submit.prevent="register">
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="error in errors" :key="error">{{ error }}</li>
+    </ul>
+  </p>
     <input v-model="name" type="text" placeholder="name" />
-    <input v-model="price" type="number" placeholder="price" />
+    <input v-model="price" type="number" placeholder="price" min="0" />
     <input v-model="imageUrl" type="text" placeholder="imageUrl" />
     <button type="submit">submit</button>
   </form>
@@ -18,9 +24,10 @@ export default {
   name: 'Admin',
   data:() => {
     return {
-      name: '',
-      price: '',
-      imageUrl: '',
+      errors: [],
+      name: null,
+      price: null,
+      imageUrl: null,
     };
   },
   methods: {
@@ -29,13 +36,29 @@ export default {
       this.$router.push('/');
     },
     register: async function() {
+      this.checkForm();
       try{
         await api.createItem({ name: this.name, price: this.price, imageUrl: this.imageUrl });
       } catch(err) {
         console.error(err);
       }
-      
     },
+    checkForm: function(e) {
+      if (this.name && this.price && this.imageUrl) {
+        return true;
+      }
+      this.errors = [];
+      if (!this.name) {
+        this.errors.push('Name required.');
+      }
+      if (!this.price) {
+        this.errors.push('Price required.');
+      }
+      if (!this.imageUrl) {
+        this.errors.push('Image URL required.');
+      }
+      e.preventDefault();
+    }
   },
 };
 </script>
